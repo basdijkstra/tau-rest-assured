e1e13a0aaf234ef98417d72f2196b2d26abee3ff
Bas Dijkstra <bas@ontestautomation.com>
2019-02-04 13:11:24 +0100

    Additional tests for chapter 2

--- src/test/java/examples/Chapter2Test.java
+++ src/test/java/examples/Chapter2Test.java
@@ -1,11 +1,34 @@
 package examples;
 
+import io.restassured.http.ContentType;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
 public class Chapter2Test {
+
+    @Test
+    public void requestUsZipCode90210_checkStatusCode_expectHttp200() {
+
+        given().
+        when().
+            get("http://zippopotam.us/us/90210").
+        then().
+            assertThat().
+            statusCode(200);
+    }
+
+    @Test
+    public void requestUsZipCode90210_checkContentType_expectApplicationJson() {
+
+        given().
+        when().
+            get("http://zippopotam.us/us/90210").
+        then().
+            assertThat().
+            contentType(ContentType.JSON);
+    }
 
     @Test
     public void requestUsZipCode90210_checkPlaceNameInResponseBody_expectBeverlyHills() {
